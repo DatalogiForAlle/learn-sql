143b2d19e887cae8e290a11d509c8e7e6e19f809
Kristian Møller Schmidt <kmollerschmidt@gmail.com>
2021-11-19 16:05:16 +0100

    added two tests

---
+++ learnsql/tests/test_views.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
+class HomeViewGetRequestTests(TestCase):
+    def test_home_view_url_exists_at_proper_location_and_uses_proper_template_1(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'learnsql/home.html')
+
+    def test_home_view_url_exists_at_proper_location_and_uses_proper_template_2(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'learnsql/home.html')
